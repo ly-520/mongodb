cd023aa9b72bc2b83048c09e8e890b0115228daf
liujitong <liujitong@yonrun.com>
2020-01-02 14:02:06 +0800

    Initial commit

---
+++ src/main/java/com/mongodb/mongodb/controller/MongodbController.java
@@ -0,0 +1,53 @@
+package com.mongodb.mongodb.controller;
+
+import com.mongodb.mongodb.dao.MongoTestDao;
+import com.mongodb.mongodb.model.MongoTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ljt
+ * @Title: MongodbController
+ * @ProjectName mongodb
+ * @Description:
+ * @Version:
+ * @date 2020/1/2 13:23
+ */
+@RestController
+public class MongodbController {
+
+    @Autowired
+    private MongoTestDao mtdao;
+
+    @GetMapping(value="/test1")
+    public void saveTest() throws Exception {
+        MongoTest mgtest=new MongoTest();
+        mgtest.setId(11);
+        mgtest.setAge(33);
+        mgtest.setName("ceshi");
+        mtdao.saveTest(mgtest);
+    }
+
+    @GetMapping(value="/test2")
+    public MongoTest findTestByName(){
+        MongoTest mgtest= mtdao.findTestByName("ceshi");
+        System.out.println("mgtest is "+mgtest);
+        return mgtest;
+    }
+
+    @GetMapping(value="/test3")
+    public void updateTest(){
+        MongoTest mgtest=new MongoTest();
+        mgtest.setId(11);
+        mgtest.setAge(44);
+        mgtest.setName("ceshi2");
+        mtdao.updateTest(mgtest);
+    }
+
+    @GetMapping(value="/test4")
+    public void deleteTestById(){
+        mtdao.deleteTestById(11);
+    }
+
+}
---
+++ src/main/java/com/mongodb/mongodb/model/MongoTest.java
@@ -0,0 +1,44 @@
+package com.mongodb.mongodb.model;/**
+ * @author ljt
+ * @Title: MongoTest
+ * @ProjectName mongodb
+ * @Description:
+ * @Version:
+ * @date 2020/1/2 13:26
+ */
+
+/**
+ * @author
+ * @description
+ * @date 2020/1/2
+ *
+ */
+public class MongoTest {
+    private Integer id;
+    private Integer age;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
